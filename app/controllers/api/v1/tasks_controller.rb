a51732b700cdc22fd2115345352631cdce62b764
Julia Grigorchuk <grigorchukj@gmail.com>
2017-02-20 16:51:23 +0200

    Add all necessary changes in db and controllers for data sending to client

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::TasksController < Api::V1::BaseController
+	def index
+		respond_with Task.all
+	end
+
+	def create
+		respond_with :api, :v1, Task.create(task_params)
+	end
+		
+	def destroy
+		respond_with Task.destroy(params[:id])
+	end
+
+	def update task = Task.find(params["id"])
+		task.update_attributes(task_params)
+		respond_with task, json: task
+	end 
+
+	private def task_params 
+		params.require(:task).permit(:id, :name, :description)
+	end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create!([{name: 'First Task', isCompleted: false, isEditable: false},
+	{name: 'Second Task', isCompleted: false, isEditable: false},
+	{name: 'Third Task', isCompleted: true, isEditable: false},
+	{name: 'Fourth Task', isCompleted: false, isEditable: false},
+	{name: 'Fifth Task', isCompleted: true, isEditable: false}])
